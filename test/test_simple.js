5b2e52736af3a7591b8eb14807eeff1dd1fe719a
Scisco <scisco7@gmail.com>
2016-10-18 12:35:00 -0400

    add tests for invalid polygon

--- test/test_simple.js
+++ test/test_simple.js
@@ -13,6 +13,18 @@ test.before('setup nock', function (t) {
   nock.back.setMode(process.env.NOCK_BACK_MODE || 'lockdown');
 });
 
+test.cb('test with invalid polygon', function (t) {
+  var key = 'simpleGet';
+  nock.back('simple-' + key + '.json', function (nockDone) {
+    var search = new Search(payload.getSelfIntersectingPolygon);
+    search.simple(function (err, response) {
+      nockDone();
+      t.is(err.message, 'Invalid Polgyon: self-intersecting');
+      t.end();
+    });
+  });
+});
+
 test.cb('root endpoint with simple GET should return 1 result', function (t) {
   var key = 'simpleGet';
   nock.back('simple-' + key + '.json', function (nockDone) {
